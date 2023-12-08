d2ec90fb3c664777f361f96b155a99ccf9901e09
bourbakiii <gabisov03912@gmail.com>
2023-12-09 01:21:28 +0300

    Installed and setted mysql and Sequelize| Connected to db

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import $auth_router from './auth/router.js';
 import express from 'express';
+import {Sequelize} from "sequelize";
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+app.use($auth_router);
+// Option 3: Passing parameters separately (other dialects)
+const $SequelizeConnection = new Sequelize('tracker', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+try {
+    await $SequelizeConnection.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-app.use($auth_router);
